2df8c1359cdfb7c1785fd69ea37840f03d73573a
phani <phanidharguttikonda0@gmail.com>
2024-06-23 00:33:54 +0530

    home page design

--- client/src/components/Patient.js
+++ client/src/components/Patient.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import css from './patient.module.css';
 
 function Patient(props) {
     return (
-        <div>
-            Iam a Patient
+        <div className={css.main}>
+            <nav>
+                <div className={css.logo}>
+                    <h2> Kannaya Clinics</h2>
+                    <h5> Where Care Meets Compassion</h5>
+                </div>
+                <div className={css.menu}>
+                    <div className={css.m}> Home </div>
+                    <div> recent-op's </div>
+                    <div> settings </div>
+                </div>
+            </nav>
+            <div className={css.body}></div>
         </div>
     );
 }
